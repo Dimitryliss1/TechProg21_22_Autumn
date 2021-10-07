01a54ecb835b7930180c95f530012efd386b0da4
Pavel Zhukov <48322035+Dimitryliss1@users.noreply.github.com>
2021-10-07 23:36:20 +0300

    Create Hero class

---
+++ LR1/Hero.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Павел Жуков on 07/10/2021.
+//
+
+#include "Hero.h"
---
+++ LR1/Hero.h
@@ -0,0 +1,14 @@
+//
+// Created by Павел Жуков on 07/10/2021.
+//
+
+#ifndef LR1_HERO_H
+#define LR1_HERO_H
+
+
+class Hero {
+
+};
+
+
+#endif //LR1_HERO_H
