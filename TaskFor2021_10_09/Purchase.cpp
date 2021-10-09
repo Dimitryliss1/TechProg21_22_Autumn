dea9a3a7ea3d76c548899311f82a78429c798f33
Pavel Zhukov <48322035+Dimitryliss1@users.noreply.github.com>
2021-10-09 10:35:46 +0300

    Create Purchase class

---
+++ TaskFor2021_10_09/Purchase.cpp
@@ -0,0 +1,100 @@
+//
+// Created by Павел Жуков on 09/10/2021.
+//
+
+#include "Purchase.h"
+
+Purchase::Purchase() {}
+
+Purchase::Purchase(Product firstProd) {
+    Counter<Product> tmp;
+    tmp.first = firstProd;
+    tmp.second = 1;
+    items.push_back(tmp);
+}
+
+const std::vector<Counter<Product>> Purchase::getPurchases() {
+    return items;
+}
+
+void Purchase::pushUnit(std::string name, int amt, const std::vector<Product>& available_prods) {
+    name = to_lower(name);
+    bool flag = false;
+    Product found;
+    for (auto & available_prod : available_prods){
+        if (available_prod.getProductName() == name){
+            flag = true;
+            found = available_prod;
+            break;
+        }
+    }
+    if (!flag) {
+        throw(NotFoundInListException("No such item in available products"));
+    }
+    for (auto & item: items){
+        if (item.first == found){
+            item.second += amt;
+            return;
+        }
+    }
+    Counter<Product>tmp;
+    tmp.first = found;
+    tmp.second = amt;
+    items.push_back(tmp);
+}
+
+void Purchase::popNUnits(std::string name, int amt, const std::vector<Product>& available_prods) {
+    name = to_lower(name);
+    bool flag = false;
+    Product found;
+    for (auto & available_prod : available_prods){
+        if (available_prod.getProductName() == name){
+            flag = true;
+            found = available_prod;
+            break;
+        }
+    }
+    if (!flag) {
+        throw(NotFoundInListException("No such item in available products"));
+    }
+    int cnt = 0;
+    for (auto & item: items){
+        if (item.first == found){
+            if (amt < item.second){
+                item.second -= amt;
+            } else {
+                items.erase(items.begin() + cnt);
+            }
+            return;
+        }
+        cnt++;
+    }
+    throw(NotFoundInListException("No such item in current receipt"));
+}
+
+void Purchase::popAllUnitsOfArticle(std::string name, std::vector<Product> available_prods) {
+    name = to_lower(name);
+    bool flag = false;
+    Product found;
+    for (auto & available_prod : available_prods){
+        if (available_prod.getProductName() == name){
+            flag = true;
+            found = available_prod;
+            break;
+        }
+    }
+    if (!flag) {
+        throw(NotFoundInListException("No such item in available products"));
+    }
+    int cnt = 0;
+    for (auto & item: items){
+        if (item.first == found){
+            items.erase(items.begin() + cnt);
+            return;
+        }
+        cnt++;
+    }
+    return;
+}
+
+
---
+++ TaskFor2021_10_09/Purchase.h
@@ -0,0 +1,39 @@
+//
+// Created by Павел Жуков on 09/10/2021.
+//
+
+#ifndef TASKFOR2021_10_09_PURCHASE_H
+#define TASKFOR2021_10_09_PURCHASE_H
+
+#include <iostream>
+#include <vector>
+
+#ifndef TASKFOR2021_10_09_PRODUCT_H
+#include "Product.h"
+#endif
+
+#ifndef TASKFOR2021_10_09_EXC_H
+#include "exc.h"
+#endif
+
+template <class T>
+struct Counter{
+    T first;
+    int second;
+};
+
+
+class Purchase {
+    std::vector<Counter<Product>> items;
+public:
+    Purchase();
+    explicit Purchase(Product firstProd);
+    const std::vector<Counter<Product>> getPurchases();
+    void pushUnit(std::string name, int amt, const std::vector<Product>& available_prods);
+    void popNUnits(std::string name, int amt, const std::vector<Product>& available_prods);
+    void popAllUnitsOfArticle(std::string name, std::vector<Product> available_prods);
+    virtual void printPurchase() = 0;
+};
+
+
+#endif //TASKFOR2021_10_09_PURCHASE_H
