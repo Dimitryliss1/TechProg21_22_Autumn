fbd6698461bafa7e52de16446a807aafc195a0ee
Pavel Zhukov <48322035+Dimitryliss1@users.noreply.github.com>
2021-11-22 00:33:28 +0300

    Implement MyArr

--- LR2/MyArr.h
+++ LR2/MyArr.h
@@ -5,10 +5,129 @@
 #ifndef LR2_MYARR_H
 #define LR2_MYARR_H
 
+#include <exception>
+#include <iostream>
+#ifndef LR2_USEFUL_H
+#include "useful.h"
+#endif
 
+template <class T>
 class MyArr {
-
+    int sz;
+    T* arr;
+public:
+    MyArr();
+    MyArr(const MyArr<T>& src);
+    MyArr(int sz);
+    ~MyArr();
+    void sort(bool comparator(T*, T*));
+    void operator >> (T* right);
+    T operator << (int right);
+    T operator[](int index) const;
+    int getsz();
 };
+
+template<class T>
+int MyArr<T>::getsz() {
+    return sz;
+}
+
+template<class T>
+MyArr<T>::MyArr(): sz(0), arr(nullptr) {
+    std::cout << "MyArr created";
+}
+
+template<class T>
+MyArr<T>::MyArr(const MyArr<T> &src): sz(src.sz) {
+    arr = new T [sz];
+    for (int i = 0; i < sz; i++){
+        arr[i] = T(src[i]);
+    }
+    std::cout << "MyArr copied" << std::endl;
+}
+
+template<class T>
+MyArr<T>::MyArr(int _sz) {
+    if (_sz <= 0){
+        sz = 0;
+        arr = nullptr;
+    } else {
+        sz = _sz;
+        arr = new T [sz];
+    }
+    std::cout << "MyArr of size " << sz << " initialized" << std::endl;
+}
+
+template<class T>
+void MyArr<T>::sort(bool comparator(T*, T*)) {
+    sort(arr, arr + sz, comparator);
+}
+
+template<class T>
+T MyArr<T>::operator[](int index) const{
+    if (index < 0 || index >= sz){
+        throw std::exception();
+    }
+    return arr[index];
+}
+
+template<class T>
+MyArr<T>::~MyArr() {
+    delete [] arr;
+    std::cout << "Deleted MyArr" << std::endl;
+}
+
+template<class T>
+void MyArr<T>::operator>>(T *right) {
+    if (sz == 0){
+        sz++;
+        arr = new T [sz];
+        arr[0] = T(&right);
+    } else {
+        std::cout << "Pass the place to insert the element.\n"
+                     "Available positions: from " << 0 << " to " << sz << "\n"
+                     "Your choice: ";
+        int res = safe_input();
+        while (res < 0 || res >= sz){
+            std::cout << "Wrong position. Try again: ";
+            res = safe_input();
+        }
+        T* tmp = arr;
+        arr = new T[++sz];
+        int i2 = 0;
+        for(int i = 0; i < sz; i++){
+            if (i == res){
+                arr[i] = T(&right);
+            } else {
+                arr[i] = T(tmp[i2++]);
+            }
+        }
+        delete [] tmp;
+    }
+}
+
+template<class T>
+T MyArr<T>::operator<<(int right) {
+    if (right < 0 || right >= sz){
+        throw std::exception();
+    }
+    T res = T(arr[right]);
+    T* tmp = arr;
+    if (sz == 1){
+        delete [] tmp;
+        arr = nullptr;
+        return res;
+    }
+    arr = new T[--sz];
+    int i2 = 0;
+    for (int i = 0; i < sz; i++){
+        if (i == right){
+            i2++;
+        }
+        arr[i] = tmp[i2++];
+    }
+    return res;
+}
 
 
 #endif //LR2_MYARR_H
