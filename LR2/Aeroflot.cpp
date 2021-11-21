91169cea7b7cbaa8853f4ccb1f2ac65796b8f062
Pavel Zhukov <48322035+Dimitryliss1@users.noreply.github.com>
2021-11-21 22:04:53 +0300

    Aeroflot done

--- LR2/Aeroflot.cpp
+++ LR2/Aeroflot.cpp
@@ -3,3 +3,73 @@
 //
 
 #include "Aeroflot.h"
+
+Aeroflot::Aeroflot(const std::string &origin, const std::string &destination, const std::string &planeType,
+                   const std::string &flightNo) : origin(origin), destination(destination), planeType(planeType),
+                                                  flightNo(flightNo) {
+    std::cout << "Constructing Aeroflot with params" << std::endl;
+}
+
+Aeroflot::Aeroflot(): origin("null"), destination("null"), planeType("null"), flightNo("null") {
+    std::cout << "Constructing Aeroflot without params" << std::endl;
+}
+
+Aeroflot::~Aeroflot() {
+    std::cout << "Destructing Aeroflot" << std::endl;
+}
+
+Aeroflot::Aeroflot(Aeroflot &a): origin(a.origin), destination(a.destination), planeType(a.planeType), flightNo(a.flightNo) {
+    std::cout << "Copying Aeroflot" << std::endl;
+}
+
+const std::string &Aeroflot::getOrigin() const {
+    return origin;
+}
+
+void Aeroflot::setOrigin(const std::string &origin) {
+    Aeroflot::origin = origin;
+}
+
+const std::string &Aeroflot::getDestination() const {
+    return destination;
+}
+
+void Aeroflot::setDestination(const std::string &destination) {
+    Aeroflot::destination = destination;
+}
+
+const std::string &Aeroflot::getPlaneType() const {
+    return planeType;
+}
+
+void Aeroflot::setPlaneType(const std::string &planeType) {
+    Aeroflot::planeType = planeType;
+}
+
+const std::string &Aeroflot::getFlightNo() const {
+    return flightNo;
+}
+
+void Aeroflot::setFlightNo(const std::string &flightNo) {
+    Aeroflot::flightNo = flightNo;
+}
+
+std::string Aeroflot::getGraphDesc() {
+    std::string tmp;
+    tmp = "\"" +
+          origin +
+          "\"" +
+          " -> " +
+          "\"" +
+          destination +
+          "\"" +
+          "[label=" +
+          "\"" +
+          flightNo +
+          "\n" +
+          planeType +
+          "\"" +
+          "];";
+    return tmp;
+}
+
--- LR2/Aeroflot.h
+++ LR2/Aeroflot.h
@@ -5,9 +5,28 @@
 #ifndef LR2_AEROFLOT_H
 #define LR2_AEROFLOT_H
 
+#include <iostream>
 
 class Aeroflot {
-
+    std::string origin;
+    std::string destination;
+    std::string planeType;
+    std::string flightNo;
+public:
+    Aeroflot(const std::string &origin, const std::string &destination, const std::string &planeType,
+             const std::string &flightNo);
+    Aeroflot();
+    Aeroflot(Aeroflot& a);
+    const std::string &getOrigin() const;
+    void setOrigin(const std::string &origin);
+    const std::string &getDestination() const;
+    void setDestination(const std::string &destination);
+    const std::string &getPlaneType() const;
+    void setPlaneType(const std::string &planeType);
+    const std::string &getFlightNo() const;
+    void setFlightNo(const std::string &flightNo);
+    std::string getGraphDesc();
+    ~Aeroflot();
 };
 
 
